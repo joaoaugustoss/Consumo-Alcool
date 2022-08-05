64a256e9b29b29c3afb931816289dbb15a811361
João Augusto <cop.joaoaugusto@gmail.com>
2022-08-05 07:39:57 -0300

    First commit

---
+++ filter.py
@@ -0,0 +1,67 @@
+import json
+import urllib.request
+import requests
+
+def get_IDs(link): # user_ID swarm, venue_ID foursquare
+    try:
+        my_request = urllib.request.urlopen(link)
+        my_HTML = my_request.read().decode("utf8")
+        ID_foursquare = my_HTML.split('href=\"https://foursquare.com/v/')[1].split('/')[1].split('\"')[0]
+        ID_swarm = my_HTML.split("https:\/\/fastly.4sqi.net\/img\/user\/")[1].split("\"id\":\"")[1].split("\"")[0]
+        return ID_swarm, ID_foursquare
+    except:
+        return "0", "0"
+
+def fsq_api(fsq_id):
+    try:
+        url = "https://api.foursquare.com/v3/places/" + fsq_id
+        headers = {
+            "Accept": "application/json",
+            "Authorization": "fsq3RAol9iTtB0ePqUDE8ljPr3ZjL/2apDmd//4jdCXlPIs="
+        }
+        response = requests.get(url, headers=headers)
+        data = json.loads(response.text)
+        cat = data["categories"][0]['name']
+        country = data["location"]['country']
+        city = data["location"]['locality']
+        state = data["location"]['region']
+        name = data["name"]
+        return cat, country, city, state, name
+    except:
+        return "0", "0", "0", "0", "0"
+
+def main():
+    file = open("database.json", "r")
+    file_print = open("data.csv", "w")
+    print("twt_id,fsq_id,swm_id,link,name,categorie,country,state,city,timestamp", file=file_print)
+    for json_file in file:
+        json_file = json_file[0:len(json_file)-1]
+        twt_id = json_file.split("\'id\': \'")[1].split("\'")[0] # tweet
+        if "\'text\': \"" in json_file:
+            text = json_file.split("\'text\': \"")[1].split("\"")[0]
+        else:
+            text = json_file.split("\'text\': \'")[1].split("\'")[0]
+        if "https://t.co" not in json_file:
+            continue
+        elif "\\n" in json_file:
+            link = text.split(" ")    
+            link = link[len(link)-1].split("\\n")
+        else:
+            link = text.split(" ")
+        link = link[len(link)-1]
+        if "…" in link or "https://t.co" not in link:
+            continue
+        time = json_file.split("\'timestamp\': \'")[1].split("\'")[0]
+        tmp = get_IDs(link)
+        swm_id = tmp[0]
+        fsq_id = tmp[1]
+        fsq = fsq_api(fsq_id)
+        cat = fsq[0]
+        loc = fsq[1]
+        city = fsq[2]
+        state = fsq[3]
+        name = fsq[4]
+        print(twt_id + "," + fsq_id + "," + swm_id + "," + link + "," + name + "," + cat + "," + loc + "," + state + "," + city + "," + time, file=file_print)
+
+if __name__ == "__main__":
+    main()
---
+++ twitter_api.py
@@ -0,0 +1,106 @@
+import requests
+import os
+import json
+from datetime import datetime
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FilteredStreamPython"
+    return r
+
+
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print(json.dumps(response.json()))
+
+
+def set_rules(delete):
+    # You can adjust the rules if needed
+    sample_rules = [
+        #{'value': 'check-in', 'has':'link', 'tag': 'foursquare'},
+        #{'value': 'foursquare.com', 'has':'link', 'tag': 'link'},
+        #{'value': 'instagram.com/explore/locations', 'has':'link', 'tag': 'instagram'},
+        {'value': 'www.swarmapp.com/c/', 'has':'link', 'tag': 'swarm'},
+        # has:geo -> retorna tweets que possuem tag de geolocalização fornecida pelo usuário
+        # lang:en -> retorna tweets que estão marcados na língua inglesa
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+
+
+def get_stream(set):
+    f = open('database.json', 'a')
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    for response_line in response.iter_lines():
+        if response_line:
+            dt = str(datetime.now())
+            json_response = json.loads(response_line)
+            date_time = {'timestamp':dt}
+            json_response['data'].update(date_time)
+            print('count: ' + str(i))
+            print(json_response, file=f)
+            print(json_response)
+            #print(json.dumps(json_response, indent=4, sort_keys=True))
+            #print(json_response['data']['id'] + "\t" + json_response['data']['text'] + "\n")
+
+
+def main():
+    rules = get_rules()
+    delete = delete_all_rules(rules)
+    set = set_rules(delete)
+    get_stream(set)
+
+
+if __name__ == "__main__":
+    main()
